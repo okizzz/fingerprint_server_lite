f3839d6c89db8e7143506a669134318b6285ba30
okizzz <shoose@protonmail.com>
2020-05-07 18:32:48 +0300

    add https

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express')
 const mongoose = require('mongoose')
 const fpRoutes = require('./routes/fproutes')
 const db = require('./config/db')
+const http = require('http');
+const https = require('https');
+
+const privateKey = fs.readFileSync('/home/aezakmi/cert/cloud-privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/home/aezakmi/cert/cloud-cert.pem', 'utf8');
+
+const credentials = {
+    key: privateKey,
+    cert: certificate
+};
 
 const PORT = process.env.PORT || 8888
 const app = express()
@@ -20,7 +30,7 @@ app.use(fpRoutes)
 async function start() {
   try {
     await mongoose.connect(db.url, db.params)
-    app.listen(PORT, () => {
+    https.createServer(credentials, app).listen(PORT, () => {
       console.log('Server has been started port:' + PORT)
     })
   } catch (e) {
