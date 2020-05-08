7512fb26648ea8961fefd402b8802d4e9f914887
okizzz <shoose@protonmail.com>
2020-05-08 17:13:30 +0300

    add local settings

--- index.js
+++ index.js
@@ -2,17 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const fpRoutes = require('./routes/fproutes')
 const db = require('./config/db')
-const http = require('http');
-const https = require('https');
-const fs = require('fs');
 
-const privateKey = fs.readFileSync('/home/aezakmi/cert/cloud-privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/home/aezakmi/cert/cloud-cert.pem', 'utf8');
-
-const credentials = {
-    key: privateKey,
-    cert: certificate
-};
 
 const PORT = process.env.PORT || 8888
 const app = express()
@@ -31,7 +21,7 @@ app.use(fpRoutes)
 async function start() {
   try {
     await mongoose.connect(db.url, db.params)
-    https.createServer(credentials, app).listen(PORT, () => {
+    app.listen(PORT, () => {
       console.log('Server has been started port:' + PORT)
     })
   } catch (e) {
