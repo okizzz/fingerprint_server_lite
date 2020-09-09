1d64924008c48e8cf2ff0a51d39294250c576a33
okizzz <47102435+okizzz@users.noreply.github.com>
2020-09-09 17:54:51 +0300

    fix cors

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ const PORT = process.env.PORT || 8888
 const app = express()
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "POST")
-    //res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE")
-    res.header("Access-Control-Allow-Headers", "Content-Type")
+    res.header("Access-Control-Allow-Origin", *);
+    res.header("Access-Control-Allow-Methods", "POST");
+    //res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
     next();
 });
 
