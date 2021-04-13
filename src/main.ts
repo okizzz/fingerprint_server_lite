7be054c81780c0127a89f67c8cb587098d52c0fd
okizzz <shoose@protonmail.com>
2021-04-13 15:45:32 +0300

    add conf https

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { readFileSync } from "fs";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: readFileSync("../certs/privkey1.pem"),
+    cert: readFileSync("../certs/cert1.pem"),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.enableCors();
   await app.listen(8888);
 }
