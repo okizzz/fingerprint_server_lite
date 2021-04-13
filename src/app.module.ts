18e4d23eb6b206a3e4c58e190a9ad7064257a32c
okizzz <shoose@protonmail.com>
2021-04-13 15:29:24 +0300

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { Fingerprint } from "./fingerprint.entity";
     TypeOrmModule.forRoot({
       type: "postgres",
       host: process.env.DB_HOST,
-      port: 5434,
+      port: 5432,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
