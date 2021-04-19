578b0ce61882dbd82fb1e86bc54f2c69cab0e608
server <server>
2021-04-19 13:33:26 +0000

    init

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,8 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { readFileSync } from "fs";
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: readFileSync("./certs/privkey1.pem"),
-    cert: readFileSync("./certs/cert1.pem"),
-  };
-  const app = await NestFactory.create(AppModule, { httpsOptions });
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(8888);
 }
