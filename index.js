4dcfa30d8bb1ab9fc07e04811fbf009a74310909
okizzz <shoose@protonmail.com>
2021-04-19 12:14:11 +0300

    init

--- index.js
+++ index.js
@@ -1,32 +1,42 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const fpRoutes = require('./routes/fproutes')
-const db = require('./config/db')
+const express = require("express");
+const mongoose = require("mongoose");
+const fpRoutes = require("./routes/fproutes");
+const db = require("./config/db");
+const http = require("http");
+const https = require("https");
+const fs = require("fs");
 
+const privateKey = fs.readFileSync("/home/aezakmi/cert/cloud-privkey.pem", "utf8");
+const certificate = fs.readFileSync("/home/aezakmi/cert/cloud-cert.pem", "utf8");
 
-const PORT = process.env.PORT || 8888
-const app = express()
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+};
+
+const PORT = process.env.PORT || 8888;
+const app = express();
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "POST")
-    //res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE")
-    res.header("Access-Control-Allow-Headers", "Content-Type")
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST");
+  //res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+  next();
 });
 
-app.use(express.json())
-app.use(fpRoutes)
+app.use(express.json());
+app.use(fpRoutes);
 
 async function start() {
   try {
-    await mongoose.connect(db.url, db.params)
-    app.listen(PORT, () => {
-      console.log('Server has been started port:' + PORT)
-    })
+    await mongoose.connect(db.url, db.params);
+    https.createServer(credentials, app).listen(PORT, () => {
+      console.log("Server has been started port:" + PORT);
+    });
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
 }
 
-start()
+start();
